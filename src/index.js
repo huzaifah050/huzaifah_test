bcf412974228d5510404951e7f48fbeaf3a2500f
huzaifah050 <huzaifah050@gmail.com>
2021-08-31 14:46:36 +0000

    Adding Bootstrap

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import reducer from './store/reducer';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const store = createStore(reducer, applyMiddleware(thunk));
 ReactDOM.render(
